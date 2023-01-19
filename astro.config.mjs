210caa12bd2f708c4ccd2ea90bfb862edbc3a1d2
Riley Paul <rileypaul96@gmail.com>
2023-01-19 07:26:41 -0800

    remove alpine JS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import alpinejs from "@astrojs/alpinejs";
 
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap(), tailwind(), alpinejs(), svelte()],
-  site: 'https://photo.rileypaul.ca'
+  integrations: [sitemap(), tailwind(), svelte()],
+  site: "https://photo.rileypaul.ca",
 });
