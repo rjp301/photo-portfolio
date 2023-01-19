1b52e9a6762c0f1791a6c1ea911ff3f446f06904
Riley Paul <rileypaul96@gmail.com>
2023-01-18 21:50:56 -0800

    add alpinejs and refactor navbar to be mobile first

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-
 import tailwind from "@astrojs/tailwind";
+
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap(), tailwind()],
+  integrations: [sitemap(), tailwind(), alpinejs()],
   site: 'https://photo.rileypaul.ca'
 });
