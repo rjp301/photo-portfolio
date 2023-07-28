e2a142ee6b3160562f7f916f89220d90c66a579b
Riley Paul <rileypaul96@gmail.com>
2023-07-28 11:46:13 -0700

    replace old site with new one

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
-import svelte from "@astrojs/svelte";
-import robotsTxt from 'astro-robots-txt'
+import react from "@astrojs/react";
 
-// https://astro.build/config
-import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap(), tailwind(), svelte(), mdx(), robotsTxt()],
-  site: "https://photo.rileypaul.ca"
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
 });
