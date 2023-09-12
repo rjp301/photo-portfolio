7fe9557035d9b5dfb5ecb88ed335335b3729e18c
Riley Paul <rileypaul96@gmail.com>
2023-09-11 19:33:01 -0700

    remove portfolio redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  redirects: {
-    "/": "/portfolio",
-  },
   integrations: [
     react(),
     tailwind({
